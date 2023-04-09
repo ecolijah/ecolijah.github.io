f29cf74469f00d8e2971ab331044d06c959f8659
ecolijah <ecolijah@gmail.com>
2023-04-09 15:55:49 -0500

    home page setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../css/Home.css";
+
+export default function Home() {
+  return (
+    <div id="Home">
+        <div className='greeting'>
+            <h1>elijah</h1>
+            <h1>lopez</h1>
+            <h2>portfolio</h2>
+            <p>computer science student at the university of houston</p>
+        </div>
+
+    </div>
+  )
+}
