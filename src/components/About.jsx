afdf6ad6d9f8c93ec682062eba2549039e9ef5de
ecolijah <ecolijah@gmail.com>
2023-04-13 12:09:32 -0500

    idk

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "../css/About.css"
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 export default function About() {
+
+  const aboutMe_pageHandler = () => {
+    
+    // route to page here
+  };
+
   return (
     <div id='About' className='aboutme'>
-      <h1>about me</h1>
+      <h1 onClick={aboutMe_pageHandler}>about me</h1>
     </div>
   )
 }
